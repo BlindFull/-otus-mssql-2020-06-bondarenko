6703d6b16f4d7935e55e90a62e0b1e134490f93a
BlindFull <encore198@gmail.com>
2020-11-24 16:40:32 +0300

    upd.3

---
+++ HW11/CLR_Demo/CLR_course/CLR.sql
@@ -0,0 +1,49 @@
+use WideWorldImporters;
+
+
+DROP FUNCTION IF EXISTS dbo.fn_SumFunc
+GO
+DROP PROCEDURE IF EXISTS dbo.usp_SayHello
+GO
+DROP ASSEMBLY IF EXISTS SimpleDemoAssembly
+GO
+
+exec sp_configure 'show advanced options', 1;
+go
+reconfigure;
+go
+
+exec sp_configure 'clr enabled', 1;
+exec sp_configure 'clr strict security', 0 
+go
+
+reconfigure;
+go
+
+alter database WideWorldImporters set trustworthy on; 
+
+
+create assembly DemoAssembly
+from 'C:\CLR_Demo\CLR_course\CLR_course\bin\Debug\CLR_course.dll'
+with permission_set = safe;  
+
+create function dbo.fn_SumFunc(@Num1 int, @Num2 int)  
+returns int
+as external name [DemoAssembly].[CLR_course.Class1].SumFunction;
+go 
+
+
+select 
+   dbo.fn_SumFunc(2,3) as [2+3], 
+   dbo.fn_SumFunc(4,5) as [4+5]
+
+
+create procedure dbo.usp_DemoName 
+(  
+    @Name nvarchar(50)
+)  
+AS EXTERNAL NAME  [DemoAssembly].[CLR_course.Class1].NameProcedure;  
+GO 
+
+exec dbo.usp_DemoName @Name = 'vlad';
+
